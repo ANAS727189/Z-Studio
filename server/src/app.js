25247c24464d6484a6036f70510e6b8c35b5c81c
ANAS727189 <anas23khan083@gmail.com>
2025-07-16 17:10:43 +0530

    chore: added render config

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,14 @@ import express from 'express';
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}));
 app.use(morgan("dev")); 
 app.use(express.json());
 
